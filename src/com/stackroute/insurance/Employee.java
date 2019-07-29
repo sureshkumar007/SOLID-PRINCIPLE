4cf7531af18abcbd147e79955281e23717925212
sureshkumar007 <sureshkumar12074@gmail.com>
2019-07-29 17:02:01 +0530

    employee updated

--- src/com/stackroute/insurance/Employee.java
+++ src/com/stackroute/insurance/Employee.java
@@ -1,29 +1,43 @@
 package com.stackroute.insurance;
 
-public class Employee
-{
-    private int emp_id;
-    private String name;
+public class Employee {
+        private int emp_id;
+        private String name;
 
-    public int getEmp_id() {
-        return emp_id;
+        public int getEmp_id () {
+            return emp_id;
+        }
+
+        public void setEmp_id ( int emp_id){
+            this.emp_id = emp_id;
+        }
+
+        public String getName () {
+            return name;
+        }
+
+        public void setName (String name){
+            this.name = name;
+        }
     }
 
-    public void setEmp_id(int emp_id) {
-        this.emp_id = emp_id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-}
 class Surveyor extends Employee
 {
 
+public  Employee employee;
+public void setEmp_id()
+{
+    this.setEmp_id(22);
+}
+public void setName()
+{
+    this.setName("suresh");
+}
 
-
+    @Override
+    public String toString() {
+        return "Surveyor{" +
+                "employee=" + employee +
+                '}';
+    }
 }
